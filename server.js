d84b17da96d3499693b2b4cdd85b855a8f3ef69e
Max Ainatchi <maxwell.ainatchi@gmail.com>
2018-07-25 17:54:55 -0400

    initial site

---
+++ server.js
@@ -0,0 +1,19 @@
+let Express = require("express");
+let renderer = require("express-render");
+let app = Express();
+
+app.set("view engine", "pug");
+app.set("views", "./src");
+
+app.use((req, res, next) => {
+	console.log(`HTTP ${req.httpVersion} ${req.method} ${req.originalUrl}`);
+	next();
+});
+
+app.use(Express.static("src"))
+app.use(renderer());
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log("Running on port " + port);
+});
---
+++ src/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": false
+    },
+    "extends": "../.eslintrc.js",
+    "globals": {
+        "angular": false,
+        "appName": false
+    }
+};
